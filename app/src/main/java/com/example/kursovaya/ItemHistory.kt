ece50f47f858e45a6f91bcc49f9317e7c9e37839
HeroInDaClub <151561922+HeroInDaClub@users.noreply.github.com>
2024-05-30 18:52:07 +0300

    Изменение типа данных Data-classa истории платежей

--- app/src/main/java/com/example/kursovaya/ItemHistory.kt
+++ app/src/main/java/com/example/kursovaya/ItemHistory.kt
@@ -2,4 +2,4 @@ package com.example.kursovaya
 
 import java.util.Date
 
-data class ItemHistory(val idPayment: Int, val indexPay: String, val sum: Int, val title: String, val date: String)
+data class ItemHistory(val idPayment: Int, val indexPay: Boolean, val sum: Int, val title: String, val date: String)
