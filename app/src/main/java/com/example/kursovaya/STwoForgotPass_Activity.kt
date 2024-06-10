6d86468f4b1ef7f3c05245cb3f1e776122a46e1f
Anton123-git <shagin.anthon@yandex.ru>
2024-06-10 22:06:56 +0300

    вфывфы

--- app/src/main/java/com/example/kursovaya/STwoForgotPass_Activity.kt
+++ app/src/main/java/com/example/kursovaya/STwoForgotPass_Activity.kt
@@ -1,12 +1,7 @@
 package com.example.kursovaya
-
-
 import android.content.Intent
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
 import android.view.View
-import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 
 class STwoForgotPass_Activity : AppCompatActivity() {
@@ -14,18 +9,9 @@ class STwoForgotPass_Activity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_stwo_forgotpass)
 
-        val emailEditText = findViewById<EditText>(R.id.editTextTextEmailAddress2)
+
         val sendOTPButton = findViewById<androidx.appcompat.widget.AppCompatButton>(R.id.btn_signup)
 
-        emailEditText.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
-
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                sendOTPButton.isEnabled = android.util.Patterns.EMAIL_ADDRESS.matcher(s.toString()).matches()
-            }
-
-            override fun afterTextChanged(s: Editable?) {}
-        })
 
         sendOTPButton.setOnClickListener {
             val intent = Intent(this, STwoOTPVerif_Activity::class.java)
